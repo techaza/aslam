1497b81b18029a10422344363c6d53e20186e137
IAM_VISHNU <vishnubs03011999@gmail.com>
2023-01-20 11:48:29 +0530

    m

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:aslam/screens/home.dart';
 import 'package:aslam/screens/otp.dart';
 import 'package:aslam/screens/page.dart';
+import 'package:aslam/screens/ph.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: page(),
+    home: login(),
   ));
 }
